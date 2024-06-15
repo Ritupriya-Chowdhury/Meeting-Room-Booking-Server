910b1061fc7ebf7d9b05b65b3fbaf2d9d1fa5f3d
Ritupriya <ritupriyabgc@gmail.com>
2024-06-15 19:16:05 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express, {Application, Request, Response} from 'express'
+import cors from 'cors'
+import router from './app/routes';
+
+
+
+const app : Application = express()
+
+
+//parser
+app.use(express.json());
+app.use(cors());
+
+
+// application routes
+// student route
+app.use('/api/ph-university', router);
+
+
+const getController=(req:Request, res:Response) => {
+  res.send('Hello World!')
+}
+
+
+app.get('/', getController);
+
+// app.use(globalErrorHandler);
+
+// //Not Found route
+// app.use(notFound);
+
+
+export default app;
